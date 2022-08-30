9091e09097efc5becb8486639b0334cda5a4d7b1
Koki NAKAMURA <koki.nakamura22@gmail.com>
2022-08-30 17:59:18 +0900

    add the terraform environment and the file

---
+++ terraform/main.tf
@@ -0,0 +1,53 @@
+terraform {
+  required_version = "1.2.8"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>4.0"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "terraform"
+  region  = "ap-northeast-1"
+}
+
+# --------------------------
+# S3 bucket
+# --------------------------
+resource "aws_s3_bucket" "s3_deploy_bucket" {
+  bucket = "tweets-collector"
+
+  versioning {
+    enabled = false
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "s3_deploy_bucket" {
+  bucket                  = aws_s3_bucket.s3_deploy_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+  depends_on = [
+    aws_s3_bucket_policy.s3_deploy_bucket
+  ]
+}
+
+resource "aws_s3_bucket_policy" "s3_deploy_bucket" {
+  bucket = aws_s3_bucket.s3_deploy_bucket.id
+  policy = data.aws_iam_policy_document.s3_deploy_bucket.json
+}
+
+data "aws_iam_policy_document" "s3_deploy_bucket" {
+  statement {
+    effect    = "Allow"
+    actions   = ["s3:GetObject", "s3:PutObject"]
+    resources = ["${aws_s3_bucket.s3_deploy_bucket.arn}/*"]
+    principals {
+      type        = "AWS"
+      identifiers = ["user arn"]
+    }
+  }
+}
