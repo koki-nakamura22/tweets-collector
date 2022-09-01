d1ab2f7bfa1919a14df5e9929ae2eb52a0baeb21
Koki NAKAMURA <koki.nakamura22@gmail.com>
2022-09-01 23:20:15 +0900

    delete unnecessary files

--- tweets-collector/chalicelib/twitter/twitter_api_info.py
+++
@@ -1,9 +0,0 @@
-from typing import Optional
-
-from chalicelib.utils.myyaml import load_yml
-
-
-def get_twitter_api_info(file_name: str, file_dir: Optional[str] = None):
-    yml_data = load_yml(file_name, file_dir)
-    return yml_data['api_key'], yml_data['api_secret_key'], yml_data[
-        'bearer_token'], yml_data['access_token'], yml_data['access_token_secret']
--- tweets-collector/chalicelib/utils/mypath.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-def get_this_file_dir():
-    return os.path.dirname(os.path.abspath(__file__))
-
-
-def get_current_path_as_abs():
-    return os.path.abspath('.')
